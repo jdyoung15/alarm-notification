1023fa6d451854f32be47215f8009545aded36e9
Jeremy Young <jedayoung@gmail.com>
2018-05-03 08:56:40 -0500

    Added tests for new date formats

--- app/src/test/java/com/example/alarmnotification/DateParserTest.java
+++ app/src/test/java/com/example/alarmnotification/DateParserTest.java
@@ -41,7 +41,57 @@ public class DateParserTest {
 
   @Test
   public void parseSlash4() {
-    assertEquals(LocalDate.of(2018, 12, 31), parser.parse("12/31"));
+    assertEquals(LocalDate.of(LocalDate.now().getYear(), 12, 31), parser.parse("12/31"));
+  }
+
+  @Test
+  public void parseDash1() {
+    assertEquals(LocalDate.of(2018, 1, 1), parser.parse("1-1-2018"));
+  }
+
+  @Test
+  public void parseDash2() {
+    assertEquals(LocalDate.of(2018, 1, 1), parser.parse("01-01-2018"));
+  }
+
+  @Test
+  public void parseDash3() {
+    assertEquals(LocalDate.of(2018, 1, 1), parser.parse("1-1-18"));
+  }
+
+  @Test
+  public void parseDash4() {
+    assertEquals(LocalDate.of(LocalDate.now().getYear(), 12, 31), parser.parse("12-31"));
+  }
+
+  @Test
+  public void parseSpaceSeparatedAbbrev1() {
+    assertEquals(LocalDate.of(2018, 1, 1), parser.parse("Jan 1 2018"));
+  }
+
+  @Test
+  public void parseSpaceSeparatedAbbrev2() {
+    assertEquals(LocalDate.of(2018, 1, 1), parser.parse("jan 1 18"));
+  }
+
+  @Test
+  public void parseSpaceSeparatedAbbrev3() {
+    assertEquals(LocalDate.of(2018, 12, 31), parser.parse("dec 31"));
+  }
+
+  @Test
+  public void parseSpaceSeparatedFull1() {
+    assertEquals(LocalDate.of(2018, 1, 1), parser.parse("january 1 2018"));
+  }
+
+  @Test
+  public void parseSpaceSeparatedFull2() {
+    assertEquals(LocalDate.of(2018, 1, 1), parser.parse("January 1 18"));
+  }
+
+  @Test
+  public void parseSpaceSeparatedFull3() {
+    assertEquals(LocalDate.of(2018, 12, 31), parser.parse("December 31"));
   }
 
 }
