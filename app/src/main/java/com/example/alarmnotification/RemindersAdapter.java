c78673d0e995e77220f0f05181014c420d15d34a
Jeremy Young <jedayoung@gmail.com>
2018-05-02 20:36:58 -0500

    Added code and images for delete button

--- app/src/main/java/com/example/alarmnotification/RemindersAdapter.java
+++ app/src/main/java/com/example/alarmnotification/RemindersAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.alarmnotification.alarms.ReminderAlarm;
@@ -29,11 +31,11 @@ public class RemindersAdapter extends RecyclerView.Adapter<RemindersAdapter.View
   // you provide access to all the views for a data item in a view holder
   public static class ViewHolder extends RecyclerView.ViewHolder {
     // each data item is just a string in this case
-    public TextView textView;
+    public LinearLayout linearLayout;
 
-    public ViewHolder(TextView textView) {
-      super(textView);
-      this.textView = textView;
+    public ViewHolder(LinearLayout linearLayout) {
+      super(linearLayout);
+      this.linearLayout = linearLayout;
     }
 
   }
@@ -45,15 +47,31 @@ public class RemindersAdapter extends RecyclerView.Adapter<RemindersAdapter.View
     TextView tv = (TextView) LayoutInflater
       .from(context)
       .inflate(R.layout.reminder_display, parent, false);
-    return new ViewHolder(tv);
+    tv.setTag("reminderText");
+
+    ImageButton ib = (ImageButton) LayoutInflater
+      .from(context)
+      .inflate(R.layout.reminder_delete_button, parent, false);
+    ib.setTag("deleteButton");
+
+    LinearLayout layout = new LinearLayout(context);
+    layout.setOrientation(LinearLayout.HORIZONTAL);
+    layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+    layout.addView(tv);
+    layout.addView(ib);
+
+    return new ViewHolder(layout);
   }
 
   // Replace the contents of a view (invoked by the layout manager)
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
     Reminder reminder = remindersDataset.get(position);
-    holder.textView.setText(DateTimeFormatter.ofPattern("M/d h:mma").format(reminder.getDateTime()) + " " + reminder.getNote());
-    holder.textView.setOnClickListener(v -> {
+    TextView tv = holder.linearLayout.findViewWithTag("reminderText");
+    tv.setText(DateTimeFormatter.ofPattern("M/d h:mma").format(reminder.getDateTime()) + " " + reminder.getNote());
+    ImageButton ib = holder.linearLayout.findViewWithTag("deleteButton");
+    ib.setOnClickListener(v -> {
       remindersDataset.remove(position);
       notifyItemRemoved(position);
       notifyItemRangeChanged(position, getItemCount());
